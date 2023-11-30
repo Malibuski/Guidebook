17cc5dce646626e24e531d3344c643c0688bbca3
Scott Welch <cscottwelch2@gmail.com>
2023-11-30 13:12:59 -0500

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Scott Welch on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
